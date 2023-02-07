bce565d2154a9c45c086f99204588c3ba53b71f6
talessantos49 <tasantos@c2r7p3.42sp.org.br>
2023-02-07 10:42:37 -0300

    ajustes no Numbers e avaliação da msilveir

--- Mini-Piscina-Python/day01/ex01/numbers.py
+++ Mini-Piscina-Python/day01/ex01/numbers.py
@@ -3,6 +3,7 @@ def numbers():
     file = open("numbers.txt", "r")
     file_string = file.read()
     print(file_string.replace("," , "\n").rstrip('\n'))
+    file.close()
 
 if __name__ =="__main__":
     numbers()
---
+++ Mini-Piscina-Python/day02/ex01/intern.py
@@ -0,0 +1,9 @@
+class intern:
+    def __init__(self, nome, idade):
+        self.nome = "My name? I’m nobody, an intern, I have no name."
+        self.idade = "29"
+
+def intern_function():
+
+if __name__ =="__main__":
+    intern_function()
