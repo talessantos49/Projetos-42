529ecd0e1b478f5101fbdd6f07b617d1f3385572
talessantos49 <tasantos@c2r7p3.42sp.org.br>
2023-02-07 18:07:36 -0300

    Primeira subida

--- Mini-Piscina-Python/day02/ex02/beverages.py
+++ Mini-Piscina-Python/day02/ex02/beverages.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 class HotBeverage:
     def __init__(self,name = "hot beverage", price = 0.30, description = "Just some hot water in a cup."):
         self.name = name
@@ -5,4 +6,24 @@ class HotBeverage:
         self.description = description
 
     def __str__(self):
-        
+        returned_str = ""
+        returned_str += "name : " + self.name + "\n"
+        returned_str += "price : " + str("{:.2f}".format(self.price)) + "\n"
+        returned_str += "description : " + self.description
+        return returned_str
+
+class Coffe(HotBeverage):
+    def __init__(self, name="coffe", price = 0.50, description = "A coffe, to stay awake."):
+        super().__init__(name = name, price = price, description=description)
+
+class Tea(HotBeverage):
+    def __init__(self, name="tea"):
+        super().__init__(name = name)
+
+if __name__ == "__main__":
+    drink = HotBeverage()
+    print(drink, "\n")
+    coffe = Coffe()
+    print(coffe, "\n")
+    tea = Tea()
+    print(tea, "\n")
