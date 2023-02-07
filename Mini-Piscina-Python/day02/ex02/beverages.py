d8a1b8059d9103fe3e2528f1bd11d7e0aaa36df3
talessantos49 <tasantos@c1r3p3.42sp.org.br>
2023-02-07 18:31:43 -0300

    beverages complete

--- Mini-Piscina-Python/day02/ex02/beverages.py
+++ Mini-Piscina-Python/day02/ex02/beverages.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
 class HotBeverage:
-    def __init__(self,name = "hot beverage", price = 0.30, description = "Just some hot water in a cup."):
+    def __init__(self,name = "hot beverage", price = 0.30 ):
         self.name = name
         self.price = price
-        self.description = description
+
+    def description(self):
+        return ("Just some hot water in a cup.")
 
     def __str__(self):
         returned_str = ""
         returned_str += "name : " + self.name + "\n"
         returned_str += "price : " + str("{:.2f}".format(self.price)) + "\n"
-        returned_str += "description : " + self.description
+        returned_str += "description : " + self.description()
         return returned_str
+    
+
 
 class Coffe(HotBeverage):
-    def __init__(self, name="coffe", price = 0.50, description = "A coffe, to stay awake."):
-        super().__init__(name = name, price = price, description=description)
+    def __init__(self, name="coffe", price = 0.50):
+        super().__init__(name = name, price = price)
+    def description(self):
+        return "A coffee, to stay awake."
 
 class Tea(HotBeverage):
     def __init__(self, name="tea"):
         super().__init__(name = name)
+
+class Chocolate(HotBeverage):
+    def __init__(self, name="chocolate", price = 0.50):
+        super().__init__(name=name, price=price)
+
+    def description(self):
+        return "Chocolate, sweet chocolate..."
+
+class Cappuccino(HotBeverage):
+    def __init__(self, name="cappuccino", price = 0.45):
+        super().__init__(name=name, price=price)
+
+    def description(self):
+        return "Un po’ di Italia nella sua tazza!"
 
 if __name__ == "__main__":
     drink = HotBeverage()
@@ -27,3 +47,7 @@ if __name__ == "__main__":
     print(coffe, "\n")
     tea = Tea()
     print(tea, "\n")
+    chocolate = Chocolate()
+    print(chocolate, "\n")
+    capputino = Cappuccino()
+    print(capputino, '\n')
