c2b59421efac9a8162fb362dec95ed450ee582bf
talessantos49 <tasantos@c1r4p1.42sp.org.br>
2023-02-09 18:31:05 -0300

    Ex01 Finalizado

--- Mini-Piscina-Python/day03/ex01/my_script.sh
+++ Mini-Piscina-Python/day03/ex01/my_script.sh
@@ -1,11 +1,6 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 pip --version
-# python -m path.py install
 
-#pip install virtualenv
-#python3 -m virtualenv my_env (é a criação do seu ambiente virtual com o nome "my_env em python3")
-#source my_env/bin/activate (Para entrar no meu ambiente virtual!)
-#deactivate (serve para sair do ambiente virtual)
 python3 -m virtualenv my_env
 
 source my_env/bin/activate
