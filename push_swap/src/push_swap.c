8aa9eb82f2baf5717b0a8e627343db6e660c06be
talessantos49 <tasantos@c1r2p2.42sp.org.br>
2023-02-13 22:12:26 -0300

    subida

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tasantos <tasantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 18:33:40 by tasantos          #+#    #+#             */
-/*   Updated: 2023/02/13 21:07:23 by tasantos         ###   ########.fr       */
+/*   Updated: 2023/02/13 22:06:13 by tasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,45 @@
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
 
-void check_arguments(int argc, char **argv)
+void    check_arguments(int argc, char **argv)
 {
     int i;
-    long int enter_argument;
+    int k;
+    int is_num;
+    int len;
+    long long int enter_argument;
 
     i = 0;
+    k = -1;
     enter_argument = 0;
+    is_num = 0;
     while (i < argc)
     {
-        enter_argument = ft_atoi(argv[i]);
+        ft_printf("teste: %i\n", argv[i][k++]);
+        len = ft_strlen(argv[i]);
+        ft_printf("len: %i\n", len);
+        while (k++ < len)
+        {
+            is_num = ft_isdigit(argv[i][k]);
+            ft_printf("is_num = %c\n",is_num);   
+        }
+        enter_argument = ft_atoi (argv[i]);
+        if (is_num == 1)
+            ft_printf("Error: Invalid type of argument, please put a number\n");
         if (enter_argument >= INT_MAX)
-            ft_printf("Error: Enter a valid argument number.");
+            enter_argument = (INT_MAX);
         if (enter_argument <= INT_MIN)
-            ft_printf("Error: Enter a valid argument number!");
+            enter_argument = INT_MIN;
+        if (enter_argument == INT_MAX)
+            ft_printf("Error: Enter a valid argument number.\n");
+        if (enter_argument == INT_MIN)
+            ft_printf("Error: Enter a valid argument number Interger Overflow.\n");
         i++;
+        k = 0;
     }
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    check_arguments(argc, argv);
+    check_arguments (argc, argv);
 }
