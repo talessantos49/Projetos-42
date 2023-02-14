dc5d4258564e9e0ecdf839adbc2f3c35f59f5341
talessantos49 <tasantos@c1r2p2.42sp.org.br>
2023-02-13 21:13:46 -0300

    new: inicio de projeto, primeira parte de validação de argumentos

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tasantos <tasantos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 20:50:08 by tasantos          #+#    #+#             */
+/*   Updated: 2023/02/13 20:52:00 by tasantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../includes/libft.h"
+
+void check_arguments(int argc, char **argv);
+
+#endif
--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -1,20 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tasantos <tasantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: tasantos <tasantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 18:33:40 by tasantos          #+#    #+#             */
-/*   Updated: 2023/01/08 05:31:41 by tasantos         ###   ########.fr       */
+/*   Updated: 2023/02/13 21:07:23 by tasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "../includes/push_swap.h"
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 
 void check_arguments(int argc, char **argv)
 {
+    int i;
+    long int enter_argument;
 
+    i = 0;
+    enter_argument = 0;
+    while (i < argc)
+    {
+        enter_argument = ft_atoi(argv[i]);
+        if (enter_argument >= INT_MAX)
+            ft_printf("Error: Enter a valid argument number.");
+        if (enter_argument <= INT_MIN)
+            ft_printf("Error: Enter a valid argument number!");
+        i++;
+    }
 }
 
 int main(int argc, char **argv)
