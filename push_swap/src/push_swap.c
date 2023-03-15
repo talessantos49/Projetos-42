e57ccac81c8b08b52db26df0c1e9f7d32655a93e
talessantos49 <talessantos49@gmail.com>
2023-03-15 12:15:51 -0300

    Finalização de testes com stacks, realizado a criação de todos os movimentos possiveis.

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -1,59 +1,226 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   push_swap.c                                        :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: tasantos <tasantos@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/02/07 18:33:40 by tasantos          #+#    #+#             */
+// /*   Updated: 2023/02/13 22:06:13 by tasantos         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "../includes/push_swap.h"
+
+// #define INT_MAX 2147483647
+// #define INT_MIN -2147483648
+
+// void    check_arguments(int argc, char **argv)
+// {
+//     int i;
+//     int k;
+//     int is_num;
+//     int len;
+//     long long int enter_argument;
+
+//     i = 0;
+//     k = -1;
+//     enter_argument = 0;
+//     is_num = 0;
+//     while (i < argc)
+//     {
+//         ft_printf("teste: %i\n", argv[i][k++]);
+//         len = ft_strlen(argv[i]);
+//         ft_printf("len: %i\n", len);
+//         while (k++ < len)
+//         {
+//             is_num = ft_isdigit(argv[i][k]);
+//             if (ft_isdigit(argv[i][k]))
+//                 ft_printf("is_num = %d\n",is_num);   
+//         }
+//         enter_argument = ft_atoi (argv[i]);
+//         if (is_num == 1)
+//             ft_printf("Error: Invalid type of argument, please put a number\n");
+//         if (enter_argument >= INT_MAX)
+//             enter_argument = (INT_MAX);
+//         if (enter_argument <= INT_MIN)
+//             enter_argument = INT_MIN;
+//         if (enter_argument == INT_MAX)
+//             ft_printf("Error: Enter a valid argument number.\n");
+//         if (enter_argument == INT_MIN)
+//             ft_printf("Error: Enter a valid argument number Interger Overflow.\n");
+//         i++;
+//         k = 0;
+//     }
+// }
+
+// int main (int argc, char **argv)
+// {
+//     check_arguments (argc, argv);
+// }
+
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tasantos <tasantos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tasantos <tasantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/07 18:33:40 by tasantos          #+#    #+#             */
-/*   Updated: 2023/02/13 22:06:13 by tasantos         ###   ########.fr       */
+/*   Created: 2023/01/30 22:33:00 by tasantos          #+#    #+#             */
+/*   Updated: 2023/03/08 19:25:08 by tasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+#include <stdio.h>
 
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
 
-void    check_arguments(int argc, char **argv)
+int	ft_is_space(char c)
 {
-    int i;
-    int k;
-    int is_num;
-    int len;
-    long long int enter_argument;
+	if ((c >= 9 && c <= 13) || c == ' ')
+		return (1);
+	return (0);
+}
 
-    i = 0;
-    k = -1;
-    enter_argument = 0;
-    is_num = 0;
+int	ft_atoi_push(const char	*str)
+{
+	// long int	i;
+	// long int	negative;
+	// long int	num;
+
+	// i = 0;
+	// num = 0;
+	// negative = 1;
+	// while (ft_is_space(str[i]))
+	// 	i++;
+	// if (str[i] == '-' || str[i] == '+')
+	// {
+	// 	if (str[i] == '-')
+	// 		negative = -negative;
+	// 	i++;
+	// }
+	// while (str[i] >= '0' && str[i] <= '9')
+	// {
+	// 	num = (num * 10) + str[i] - '0';
+	// 	i++;
+	// }
+	// return (num * negative);
+    long long int	result;
+	int				i;
+	int				s;
+
+	s = 1;
+	i = 0;
+	while (str[i] == 32 || (str[i] >= 7 && str[i] <= 13))
+		i++;
+	if ((str[i] == '-' || str[i] == '+')
+		&& (str[i + 1] == '-' || str[i +1] == '+'))
+		return (0);
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			s = -1;
+		i++;
+	}
+	result = 0;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (s * result);
+}
+
+
+int	check_max_min(char *str)
+{
+	const long long	result = ft_atoi_push(str);
+
+	if (result > 2147483647 || result < -2147483648)
+	{
+		write(1, "Error\n", 7);
+		return (0);
+	}
+	return (1);
+}
+
+int	check_numbers(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+		i++;
+	while (str[i] == 32)
+		i++;
+	if (str[i] == '\0')
+		return (1);
+	write(1, "Error\n", 7);
+	return (0);
+}
+
+void	checkers(char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (check_numbers(argv[i]) == 0)
+			exit(0);
+		if (check_max_min(argv[i]) == 0)
+			exit(0);
+		i++;
+	}
+}
+
+void    check_argv(int argc, char *argv[])
+{
+    int    	i = 1;
+	// ssize_t i = 0;
+    // int 	k = 0;
+    // int    	atoi = 0;
+	// char 	*int_Max = "-2147483648";
+
     while (i < argc)
     {
-        ft_printf("teste: %i\n", argv[i][k++]);
-        len = ft_strlen(argv[i]);
-        ft_printf("len: %i\n", len);
-        while (k++ < len)
-        {
-            is_num = ft_isdigit(argv[i][k]);
-            ft_printf("is_num = %c\n",is_num);   
-        }
-        enter_argument = ft_atoi (argv[i]);
-        if (is_num == 1)
-            ft_printf("Error: Invalid type of argument, please put a number\n");
-        if (enter_argument >= INT_MAX)
-            enter_argument = (INT_MAX);
-        if (enter_argument <= INT_MIN)
-            enter_argument = INT_MIN;
-        if (enter_argument == INT_MAX)
-            ft_printf("Error: Enter a valid argument number.\n");
-        if (enter_argument == INT_MIN)
-            ft_printf("Error: Enter a valid argument number Interger Overflow.\n");
+        check_max_min(argv[i]);
+        // k = 0;
+		// ft_printf("O primeiro Argumento : ");
+		// ft_printf(argv[i]);
+		// ft_printf("\n");
+		// ft_printf("O valor de Int Max : ");
+		// ft_printf(int_Max);
+		// ft_printf("\n");
+		// if (argv[i] == int_Max)
+		// 	ft_printf("O valor de int maximo negativo está sendo usado!");
+        // while (argv[i][k])
+        // {
+        //     if (!ft_isdigit(argv[i][k]))
+        //         ft_printf("Is not a number\n");
+        //     // ft_printf("%d Its a number\n",ft_isdigit(argv[i][k]));
+        //     k++;
+        // }
+      	// atoi = ft_atoi_push(argv[i]);
+        // ft_printf("The number of the atoi is %d\n", atoi);
         i++;
-        k = 0;
     }
 }
 
-int main (int argc, char **argv)
+int    main(int argc, char **argv)
+// int	main()
 {
-    check_arguments (argc, argv);
+
+    if (argc < 2)
+        return (ft_printf("Erroo\n"));
+    check_argv(argc, argv);
+    
+    return (0);
 }
--- push_swap/src/stacks.c
+++ push_swap/src/stacks.c
@@ -8,14 +8,14 @@ typedef struct s_node{
 }t_node;
 
 typedef struct s_stack{
-	t_node *topo;
-    t_node *fundo;
+	t_node *first;
+    t_node *last;
 	int tam;
 } t_stack;
 
 void criar_pilha(t_stack *stackA){
-	stackA->topo = NULL;
-    stackA->fundo = NULL;
+	stackA->first = NULL;
+    stackA->last = NULL;
 	stackA->tam = 0;
 }
 
@@ -23,7 +23,7 @@ void imprimir_pilha(t_stack *stack, char c){
 	ft_printf("\n-------------- PILHA - ");
 	ft_printf("%c",c);
 	ft_printf(" tam: %d----------------\n", stack->tam);
-	t_node *aux = stack->topo;
+	t_node *aux = stack->first;
 	while (aux){
 		ft_printf("%d\t",aux->data);
 		aux = aux->next;
@@ -31,13 +31,57 @@ void imprimir_pilha(t_stack *stack, char c){
 	ft_printf("\n-------------- FIM DA PILHA ---------\n");
 }
 
+void inserir_na_fila(t_stack *stack, int num){
+	t_node *new = malloc(sizeof(t_node));
+	// t_node *aux = malloc(sizeof(t_node));
+
+	if (new){
+		new->data = num;
+		new->next = NULL;
+		if (stack->first == NULL){
+			stack->first = new;
+			stack->last = new;
+		}
+		else{
+			stack->last->next = new;
+			stack->last = new;
+		}
+		stack->tam++;
+	}
+	else
+		ft_printf("Erro ao alocar memoria!\n");
+}
+
+t_node *remover_da_fila(t_stack *stack){
+	t_node *remover = NULL;
+	if (stack->first){
+		remover = stack->first;
+		stack->first = remover->next;
+		stack->tam--;
+	}
+	else
+		ft_printf("\tFila Vazia\n");
+	return remover;
+}
+
+void imprimir_fila(t_stack *stack){
+	t_node *aux = malloc(sizeof(t_node));
+	aux = stack->first;
+	ft_printf("\n\t ------ Fila -------\n\t");
+	while (aux){
+		ft_printf("%d ", aux->data);
+		aux = aux->next;
+	}
+	ft_printf("\n\t ------ Fim da FILA ------ \n");
+}
+
 void empilhar(t_stack *stack, int value)
 {
 	t_node *new = malloc(sizeof(t_node));
 	if(new){
 		new->data = value;
-		new->next = stack->topo;
-		stack->topo = new;
+		new->next = stack->first;
+		stack->first = new;
 		stack->tam++;
 	}
 	else
@@ -45,9 +89,9 @@ void empilhar(t_stack *stack, int value)
 }
 
 t_node *desempilhar(t_stack *stack){
-	if (stack->topo){
-		t_node *remove = stack->topo;
-		stack->topo = remove->next;
+	if (stack->first){
+		t_node *remove = stack->first;
+		stack->first = remove->next;
 		stack->tam--;
 		return remove;
 	}
@@ -56,20 +100,74 @@ t_node *desempilhar(t_stack *stack){
 	return NULL;
 }
 
+void remover_lista(t_stack *stack, int valor){
+	t_node *inicio = stack->first;
+	t_node *noARemover = NULL;
+
+	if (inicio != NULL && stack->first->data == valor){
+		noARemover = stack->first;
+		stack->first = noARemover->next;
+		if(stack->first == NULL)
+			stack->last = NULL;
+	}
+	else {
+		while (inicio != NULL && inicio->next != NULL && inicio->next->data != valor){
+			inicio = inicio->next;
+		}
+		if (inicio != NULL && inicio->next != NULL){
+			noARemover = inicio->next;
+			inicio->next = noARemover->next;
+			if (inicio->next == NULL)
+				stack->last = inicio;
+		}
+	}
+	if (noARemover){
+		free(noARemover);
+		stack->tam--;
+	}
+}
+
+void remover_no_fim(t_stack *stack)
+{
+    t_node *aux = malloc(sizeof(t_node));
+
+    if(aux){
+         //é o primeiro?
+        if (stack->first)
+		{
+	        aux = stack->first;
+            while(aux->next)
+			{
+                aux = aux->next;
+				ft_printf("\nO valor retornado é: %d\n", aux->data);
+			}
+			t_node *remove = aux->next;
+			aux->next = remove->next;
+		}
+        else{
+			ft_printf("Pilha Vazia");
+        }
+
+        stack->tam--;
+    }
+    else
+        ft_printf("Erro ao alocar memoria!\n");
+}
+
 void shift_up(t_stack *stack)
 {
 	t_node *aux = malloc(sizeof(t_node));
 	t_node *new = malloc(sizeof(t_node));
 
 	if (new){
-		new->data = stack->topo->data;
+		new->data = stack->first->data;
 		new->next = NULL;
 		desempilhar(stack);
 		//é o primeiro?
-		if (stack->topo == NULL)
-			stack->topo = new;
+		if (stack->first == NULL)
+			stack->first = new;
 		else{
-			aux = stack->topo;
+			aux = stack->first;
 			while (aux->next)
 				aux = aux->next;
 			aux->next = new;
@@ -85,63 +183,49 @@ void shift_up_rr(t_stack *stackA, t_stack *stackB)
 	shift_up(stackA);
 	shift_up(stackB);
 }
+
 void inserir_no_inicio(t_stack *stack, int num)
 {
     t_node *new = malloc(sizeof(t_node));
 
     if(new){
         new->data = num;
-        new->next = stack->topo;
-        stack->topo = new;
+		if (stack->first == NULL){
+			new->next = NULL;
+			stack->first = new;
+			stack->last = new;
+		}
+		else {
+			new->next = stack->first;
+			stack->first = new;
+		}
         stack->tam++;
     }
     else
         ft_printf("erro alocar Memoria!\n");
 }
-void remover_no_fim(t_stack *stack)
-{
-    t_node *new = malloc(sizeof(t_node));
-    t_node *aux = malloc(sizeof(t_node));
-
-    // if (stack->topo){
-    //     t_node *remove = 
-    // }
-    if(new){
-        new->data = 0;
-        new->next = NULL;
-
-        //é o primeiro?
-        if (stack->topo == NULL)
-            stack->topo = new;
-        else{
-            aux = stack->topo;
-            while(aux->next)
-                aux = aux->next;
-            aux->next=new;
-        }
-        stack->tam--;
-    }
-    else
-        ft_printf("Erro ao alocar memoria!\n");
-}
 
 void inserir_no_fim(t_stack *stack, int num)
 {
     t_node *new = malloc(sizeof(t_node));
-    t_node *aux = malloc(sizeof(t_node));
+    // t_node *aux = malloc(sizeof(t_node));
 
     if(new){
         new->data = num;
         new->next = NULL;
 
         //é o primeiro?
-        if (stack->topo == NULL)
-            stack->topo = new;
+        if (stack->first == NULL){
+            stack->first = new;
+			stack->last = new;
+		}
         else{
-            aux = stack->topo;
-            while(aux->next)
-                aux = aux->next;
-            aux->next=new;
+			stack->last->next = new;
+			stack->last = new;
+            // aux = stack->first;
+            // while(aux->next)
+            //     aux = aux->next;
+            // aux->next=new;
         }
         stack->tam++;
     }
@@ -150,89 +234,34 @@ void inserir_no_fim(t_stack *stack, int num)
 }
 
 void shift_down(t_stack *stack)
-{
-	t_node *aux = malloc(sizeof(t_node));
-	t_node *new = malloc(sizeof(t_node));
-
-	if (new){
-		new->data = stack->topo->data;
-		new->next = NULL;
-        desempilhar(stack);
-		//é o primeiro?
-		if (stack->topo == NULL)
-			stack->topo = new;
-		else{
-			aux = stack->topo;
-			while (aux->next)
-				aux = aux->next;
-			aux->next = new;
-		}
-		stack->tam++;
-	}
-	else
-		ft_printf("Erro ao alocar Memoria!\n");
+{	
+	int aux;
+	aux = stack->last->data;
+	remover_lista(stack, stack->last->data);
+	inserir_no_inicio(stack, aux);
 }
 
-// void inserir_no_fim(t_stack *stack, int num)
-// {
-//     t_node *new = malloc(sizeof(t_node));
-// 	t_node *aux = malloc(sizeof(t_node));
-	
-// 	if (new){
-// 		new->data = value;
-// 		new->next = NULL;
-		
-// 		//é o primeiro?
-// 		if (stack->topo == NULL)
-// 			stack->topo = new;
-// 		else {
-// 			aux = stack->topo;
-// 			while(aux->next)
-// 				aux = aux->next;
-// 			aux->next = new;
-// 		}
-//         stack->tam++;
-// 	}
-// 	else
-// 		ft_printf("Erro ao alocar memoria!\n");
-// }
-
-// void inserir_no_fim(t_node **stack, int value)
-// {
-// 	t_node *new = malloc(sizeof(t_node));
-// 	t_node *aux = malloc(sizeof(t_node));
-	
-// 	if (new){
-// 		new->data = value;
-// 		new->next = NULL;
-		
-// 		//é o primeiro?
-// 		if (*stack == NULL)
-// 			*stack = new;
-// 		else {
-// 			aux = *stack;
-// 			while(aux->next)
-// 				aux = aux->next;
-// 			aux->next = new;
-// 		}
-// 	}
-// 	else
-// 		ft_printf("Erro ao alocar memoria!\n");
-// }
+void shift_down_rr(t_stack *stackA, t_stack *stackB)
+{	
+	shift_down(stackA);
+	shift_down(stackB);
+}
 
 void push_b(t_stack *stackA, t_stack *stackB){
-	t_node *remover = NULL;
+	// t_node *remover = NULL;
 
-	empilhar(stackB, stackA->topo->data);
-	remover = desempilhar(stackA);
+	empilhar(stackB, stackA->first->data);
+	// remover = desempilhar(stackA);
+	desempilhar(stackA);
 	ft_printf("\n--------------------------------------------------\n");
 }
 
 void push_a(t_stack *stackA, t_stack *stackB){
-	t_node *remover = NULL;
+	// t_node *remover = NULL;
 
-	empilhar(stackA, stackB->topo->data);
-	remover = desempilhar(stackB);
+	empilhar(stackA, stackB->first->data);
+	// remover = desempilhar(stackB);
+	desempilhar(stackB);
 	ft_printf("\n--------------------------------------------------\n");
 }
 
@@ -270,80 +299,108 @@ int	main()
 	ft_printf("****************** Criar as Pilhas ******************\n");
 	criar_pilha(&stackA);
 	criar_pilha(&stackB);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-	ft_printf("\n****************** Empilhar Stack A ******************\n");
-	empilhar(&stackA, 1);
-	empilhar(&stackA, 2);
-	empilhar(&stackA, 3);
-	imprimir_pilha(&stackA, 'A');
-	ft_printf("\n****************** Empilhar Stack B ******************\n");
-	empilhar(&stackB, 4);
-	empilhar(&stackB, 5);
-	empilhar(&stackB, 6);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-	ft_printf("\n****************** Push AB ******************\n");
-	push_b(&stackA, &stackB);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-	ft_printf("\n****************** Push BA ******************\n");
-	push_a(&stackA, &stackB);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-	ft_printf("\n****************** Swap A ******************\n");
-	swap(&stackA);
-	imprimir_pilha(&stackA, 'A');
-	ft_printf("\n****************** Swap B ******************\n");
-	swap(&stackB);
-	imprimir_pilha(&stackB, 'B');
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+	// // ft_printf("\n****************** Empilhar Stack A ******************\n");
+	// // empilhar(&stackA, 1);
+	// // empilhar(&stackA, 2);
+	// // empilhar(&stackA, 3);
+	// // imprimir_pilha(&stackA, 'A');
+	// // ft_printf("\n****************** Empilhar Stack B ******************\n");
+	// // empilhar(&stackB, 4);
+	// // empilhar(&stackB, 5);
+	// // empilhar(&stackB, 6);
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+	// // ft_printf("\n****************** Push AB ******************\n");
+	// // push_b(&stackA, &stackB);
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+	// // ft_printf("\n****************** Push BA ******************\n");
+	// // push_a(&stackA, &stackB);
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+	// // ft_printf("\n****************** Swap A ******************\n");
+	// // swap(&stackA);
+	// // imprimir_pilha(&stackA, 'A');
+	// // ft_printf("\n****************** Swap B ******************\n");
+	// // swap(&stackB);
+	// // imprimir_pilha(&stackB, 'B');
 	
-	ft_printf("\n****************** Swap AB ******************\n");
-	swap_ab(&stackA, &stackB);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
+	// // ft_printf("\n****************** Swap AB ******************\n");
+	// // swap_ab(&stackA, &stackB);
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
 	
-	ft_printf("\n****************** Rotate A ******************\n");
-	shift_up(&stackA);
-	imprimir_pilha(&stackA, 'A');
+	// // ft_printf("\n****************** Rotate A ******************\n");
+	// // shift_up(&stackA);
+	// // imprimir_pilha(&stackA, 'A');
 
-	ft_printf("\n****************** Rotate A ******************\n");
-	shift_up(&stackA);
-	imprimir_pilha(&stackA, 'A');
+	// // ft_printf("\n****************** Rotate A ******************\n");
+	// // shift_up(&stackA);
+	// // imprimir_pilha(&stackA, 'A');
 
-	ft_printf("\n****************** Rotate A ******************\n");
-	shift_up(&stackA);
-	imprimir_pilha(&stackA, 'A');
+	// // ft_printf("\n****************** Rotate A ******************\n");
+	// // shift_up(&stackA);
+	// // imprimir_pilha(&stackA, 'A');
 
-	// ft_printf("\n****************** shift_up_RR ******************\n");
-	// imprimir_pilha(&stackA, 'A');
-	// imprimir_pilha(&stackB, 'B');
-	// shift_up_rr(&stackA, &stackB);
-	// imprimir_pilha(&stackA, 'A');
-	// imprimir_pilha(&stackB, 'B');
-    ft_printf("\n****************** shift_down_RRR ******************\n");
-    shift_down(&stackA);
-    shift_down(&stackB);
-	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-    ft_printf("\n****************** inserir no inicio ******************\n");
-    inserir_no_inicio(&stackA, 25);
-    inserir_no_inicio(&stackB, 1235);
-   	imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-    ft_printf("\n****************** inserir no fim ******************\n");
-    inserir_no_fim(&stackA, 690);
-    inserir_no_fim(&stackB, 18);
-    imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-    ft_printf("\n****************** desempulhar ******************\n");
-    desempilhar(&stackA);
-    desempilhar(&stackB);
-    imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
-    ft_printf("\n****************** Remover no fim ******************\n");
-    remover_no_fim(&stackA);
-    remover_no_fim(&stackB);
-    imprimir_pilha(&stackA, 'A');
-	imprimir_pilha(&stackB, 'B');
+	// // // ft_printf("\n****************** shift_up_RR ******************\n");
+	// // // imprimir_pilha(&stackA, 'A');
+	// // // imprimir_pilha(&stackB, 'B');
+	// // // shift_up_rr(&stackA, &stackB);
+	// // // imprimir_pilha(&stackA, 'A');
+	// // // imprimir_pilha(&stackB, 'B');
+    // // ft_printf("\n****************** shift_down_RRR ******************\n");
+    // // shift_down(&stackA);
+    // // shift_down(&stackB);
+	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+    // // ft_printf("\n****************** inserir no inicio ******************\n");
+    // // inserir_no_inicio(&stackA, 25);
+    // // inserir_no_inicio(&stackB, 1235);
+   	// // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+    // // ft_printf("\n****************** inserir no fim ******************\n");
+    // // inserir_no_fim(&stackA, 690);
+    // // inserir_no_fim(&stackB, 18);
+    // // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+    // // ft_printf("\n****************** desempulhar ******************\n");
+    // // desempilhar(&stackA);
+    // // desempilhar(&stackB);
+    // // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+    // // ft_printf("\n****************** Remover no fim ******************\n");
+    // // remover_no_fim(&stackA);
+    // // remover_no_fim(&stackB);
+    // // imprimir_pilha(&stackA, 'A');
+	// // imprimir_pilha(&stackB, 'B');
+	// ft_printf("\n***********************FILA ************************\n");
+	// imprimir_fila(&stackA);
+	// imprimir_fila(&stackB);
+	// inserir_na_fila(&stackA, 1);
+	// inserir_na_fila(&stackB, 1);
+	// inserir_na_fila(&stackA, 2);
+	// inserir_na_fila(&stackB, 2);
+	// inserir_na_fila(&stackA, 3);
+	// inserir_na_fila(&stackB, 3);
+	// // imprimir_fila(&stackA);
+	// // imprimir_fila(&stackB);
+	// // remover_lista(&stackA, stackA.last->data);
+	// // remover_lista(&stackB, stackB.last->data);
+	// imprimir_fila(&stackA);
+	// imprimir_fila(&stackB);
+	// ft_printf("\n******************************SHIFT DOWN*********************\n");
+	// shift_down(&stackA);
+	// shift_down(&stackB);
+	// imprimir_fila(&stackA);
+	// imprimir_fila(&stackB);
+	// shift_down(&stackA);
+	// shift_down(&stackB);
+	// imprimir_fila(&stackA);
+	// imprimir_fila(&stackB);
+	// ft_printf("\n******************************SHIFT DOWN*********************\n");
+	// shift_down_rr(&stackA, &stackB);
+	// imprimir_fila(&stackA);
+	// imprimir_fila(&stackB);
 }
