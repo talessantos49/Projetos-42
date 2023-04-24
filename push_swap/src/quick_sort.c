2c7ebb79d54354d57d1ed21f3d9faaeb793589f2
talessantos49 <talessantos49@gmail.com>
2023-04-24 12:09:04 -0300

    Subida para alterar sistema operacional

--- push_swap/src/quick_sort.c
+++ push_swap/src/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 22:33:00 by tasantos          #+#    #+#             */
-/*   Updated: 2023/04/22 15:57:38 by marvin           ###   ########.fr       */
+/*   Updated: 2023/04/24 11:49:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,32 @@ int	lowest_number(t_stack *stack)
 	return (min);
 }
 
+// int	*lowest_ten(t_stack *stack)
+// {
+// 	t_node	*aux;
+// 	int		min;
+// 	int		*array;
+// 	int		i;
+
+// 	i = 0;
+// 	array = calloc(sizeof(int), 10);
+// 	if (!stack->first->next)
+// 		return (0) ;
+// 	min = stack->first->data;
+// 	aux = stack->first;
+// 	array[i] = min;
+// 	while (aux->next)
+// 	{
+// 		if (min > aux->next->data)
+// 			min = aux->next->data;
+// 		aux = aux->next;
+// 		array[i] = min;
+// 		i++;
+// 	}
+// 	return (min);
+// }
+
+
 int	index_nodes(t_stack *stack)
 {
 	t_node	*aux;
@@ -76,6 +102,25 @@ int	index_nodes(t_stack *stack)
 	while (aux->next)
 	{
 		if (aux->data == low_number)
+			break;
+		position_number++;
+		aux = aux->next;
+	}
+	return (position_number);
+}
+
+int	inverted_index_nodes(t_stack *stack)
+{
+	t_node	*aux;
+	int		big_number;
+	int		position_number;
+
+	big_number = bigest_number(stack);
+	aux = stack->first;
+	position_number = 0;
+	while (aux->next)
+	{
+		if (aux->data == big_number)
 			break;
 		position_number++;
 		aux = aux->next;
@@ -113,6 +158,36 @@ int	ordenaded(t_stack *stack)
 			return (0);
 		aux= aux->next;
 	}
+	return (1);
+}
+
+int	anti_ordenaded(t_stack *stack)
+{
+	t_node	*aux;
+	int		value;
+
+	if (!stack->first)
+		return (1);
+	aux = stack->first;
+	value = aux->data;
+	while (aux->next)
+	{
+		if (value < aux->next->data)
+		{
+			imprimir_pilha(stack, 'a');
+			ft_printf("valor:[%d]\n", aux->data);
+			value = aux->next->data;
+		}
+		else
+		{
+			ft_printf("valor:[%d]\n", aux->data);
+			return (0);
+		}
+		aux= aux->next;
+	}
+	ft_printf("Estou ordenado!\n\n");
+	imprimir_pilha(stack, 'a');
+	exit(1);
 	return (1);
 }
 
@@ -157,62 +232,43 @@ int	stack_len(t_stack *stack)
 	return (i);
 }
 
-// void	pivot(t_stack *stacka, t_stack *stackb)
-// {
-// 	t_node	*aux;
-// 	t_node	*aux_temp;
-// 	int		pivot_number;
-// 	// int		len;
+void	push_rotate(t_stack *stacka, t_stack *stackb)
+{
+	t_node	*auxiliar;
+	int		low_number;
+	int		position_number;
+	int		len_stack;
 
-// 	pivot_number = 0;
-// 	aux = stacka->first;
-// 	aux_temp = aux;
-// 	// len = stack_len(stacka);
-// 	// aux1 = malloc(sizeof(t_node));
-// 	if (ordenaded(stacka))
-// 	{
-// 		push_a(stacka, stackb);
-// 		shift_up_ra(stacka);
-// 		// ft_printf("Entrou ordenado!\n");
-// 		// pivot(stacka, stackb);
-// 		return ;
-// 	}
-// 	// else 
-// 	// 	shift_down_rra(stacka);
-// 	if (aux->data > lowest_number(stacka) && aux->data < bigest_number(stacka))
-// 		pivot_number = aux->data;
+	push_a(stacka, stackb);
+	
+	low_number = lowest_number(stackb);
+	position_number = index_nodes(stackb);
+	len_stack = stack_len(stackb);
+	auxiliar = stackb->first;
+	if (auxiliar->data == low_number)
+		shift_up_ra(stacka);
+	else if (position_number > (len_stack /2))
+		shift_up_rr(stacka, stackb);
+	else
+	{
+		shift_up_ra(stacka);
+		shift_down_rrb(stackb);
+	}
+}
+
+// void	rotate_direction(t_stack *stack)
+// {
+// 	int	position_number;
+// 	int	index_position;
+
+// 	position_number = index_nodes(stack);
+// 	index_position = stack_len(stack);
+// 	if (position_number > (index_position / 2))
+// 		shift_down_rrb(stack);
 // 	else
-// 	{
-// 		swap_a(stacka);
-// 		pivot_number = aux->data;
-// 	}
-// 	while (1)
-// 	{
-// 		if (!(aux->next == NULL))
-// 			aux_temp = aux->next;
-// 		if (aux == NULL)
-// 			break;
-// 		if (pivot_number < aux->next->data)
-// 		{
-// 			swap_a(stacka);
-// 			push_b(stacka, stackb);
-// 		}
-// 		else if (pivot_number == aux->data)
-// 		{
-// 			// ft_printf("Quebrou aqui!!");
-// 			break;
-// 		}
-// 		else
-// 			shift_up_ra(stacka);
-// 		if (aux_temp == NULL)
-// 			break;
-// 		else
-// 			aux = aux_temp;
-// 	}
-// 	// free(aux1);
-// 	// ft_printf("\n\n\nFora do While ---fim\n\n");
-// 	return ;
+// 		shift_up_rb(stack);
 // }
+
 void	rotate_direction(t_stack *stack)
 {
 	int	position_number;
@@ -221,6 +277,19 @@ void	rotate_direction(t_stack *stack)
 	position_number = index_nodes(stack);
 	index_position = stack_len(stack);
 	if (position_number > (index_position / 2))
+		shift_down_rra(stack);
+	else
+		shift_up_ra(stack);
+}
+
+void	inverted_rotate_direction(t_stack *stack)
+{
+	int	position_number;
+	int	len_stack;
+
+	position_number = inverted_index_nodes(stack);
+	len_stack = stack_len(stack);
+	if (position_number > (len_stack / 2))
 		shift_down_rrb(stack);
 	else
 		shift_up_rb(stack);
@@ -237,8 +306,9 @@ void core_ordenation(t_stack *stacka, t_stack *stackb)
 		low_number = lowest_number(stackb);
 		if (low_number == auxiliar->data)
 		{
-			push_a(stacka, stackb);
-			shift_up_ra(stacka);
+			push_rotate(stacka, stackb);
+			// push_a(stacka, stackb);
+			// shift_up_ra(stacka);
 		}
 		else
 			rotate_direction(stackb);
@@ -248,15 +318,122 @@ void core_ordenation(t_stack *stacka, t_stack *stackb)
 	}
 }
 
+void inverted_core_ordenation(t_stack *stacka, t_stack *stackb)
+{
+	t_node	*auxiliar;
+	int		big_number;
+
+	auxiliar = stackb->first;
+	while (auxiliar)
+	{
+		big_number = bigest_number(stackb);
+		if (big_number == auxiliar->data)
+		{
+			// push_rotate(stacka, stackb);
+			push_a(stacka, stackb);
+			// shift_up_ra(stacka);
+		}
+		else
+			inverted_rotate_direction(stackb);
+		auxiliar = stackb->first;
+		if (auxiliar->next == NULL)
+			break;
+	}
+}
+
+int		number_pivot(t_stack *stacka, t_stack *stackb)
+{
+	int	pivot_number;
+
+	pivot_number = 0;
+	if (stack_len(stacka) <= 2 && stacka->first->next != NULL)
+	{
+		imprimir_pilha(stacka, 'a');
+		imprimir_pilha(stackb, 'b');
+		push_b(stacka, stackb);
+		imprimir_pilha(stacka, 'a');
+		imprimir_pilha(stackb, 'b');
+		pivot_number = stacka->first->data;
+	}
+	else if (stacka->last->data != lowest_number(stacka) && stacka->last->data != bigest_number(stacka))
+		pivot_number = stacka->last->data;
+	else if (stacka->first->data != lowest_number(stacka) && stacka->first->data != bigest_number(stacka))
+	{
+		pivot_number = stacka->first->data;
+		shift_up_ra(stacka);
+	}
+	return (pivot_number);
+}
+void	choice_pivot(t_stack *stacka, t_stack *stackb)
+{
+	t_node	*aux;
+	// t_node	*aux_temp;
+	int	pivot_number;
+
+	aux = stacka->first->next;
+    // aux_temp = aux;
+	while (aux)
+	{
+		if (invert_ordenaded(stackb))
+			if (stacka->first->next == NULL)
+				return ;
+		pivot_number = number_pivot(stacka, stackb);
+		if (stacka->first->next != NULL)
+		{
+			if (stacka->first->data == pivot_number)
+				// push_b(stacka, stackb);
+				return ;
+			else if (stacka->first->data > pivot_number)
+				push_b(stacka, stackb);
+			else if (stacka->first->data < pivot_number)
+			{
+				ft_printf("Pivot[%d]\tstacka->first[%d]\n", pivot_number, stacka->first->data);
+				shift_up_ra(stacka);
+				imprimir_pilha(stacka, 'a');
+				imprimir_pilha(stackb,'b');
+			}
+				// rotate_direction(stacka);
+			if (stacka->first->next == NULL)
+				return ;
+		}
+		else
+		{
+			push_b(stacka, stackb);
+			// ft_printf("Pivot[%d]\tstacka->first[%d]\n", pivot_number, stacka->first->data);
+			imprimir_pilha(stacka, 'a');
+			imprimir_pilha(stackb,'b');
+			return ;
+		}
+		aux = aux->next;
+	}
+	// ft_printf("Pivot[%d]\tstacka->first[%d]\n", pivot_number, stacka->first->data);
+	// imprimir_pilha(stacka, 'a');
+	// imprimir_pilha(stackb,'b');
+	choice_pivot(stacka, stackb);
+	return ;
+}
+
+void	returned_a (t_stack *stacka, t_stack *stackb)
+{
+	t_node	*auxiliar;
+
+	auxiliar = stackb->first->next;
+	// ft_printf("Estou aqui!\n");
+	while (auxiliar)
+	{
+		push_a(stacka, stackb);
+		auxiliar = auxiliar->next;
+	}
+	push_a(stacka, stackb);
+	// return ;
+	exit(1);
+}
+
 void pivot(t_stack *stacka, t_stack *stackb)
 {
     t_node  *aux;
     t_node  *aux_temp;
-	t_node	*auxer;
-	t_node	*auxiliar;
     int     pivot_number;
-	int		low_number;
-	int		big_number;
 
     pivot_number = 0;
     aux = stacka->first->next;
@@ -286,9 +463,7 @@ void pivot(t_stack *stacka, t_stack *stackb)
 			push_b(stacka, stackb);
 		}
 		else if (pivot_number == aux->data)
-		{
 			break;
-		}
 		else
 			shift_up_ra(stacka);
 		if (aux_temp == NULL)
@@ -297,64 +472,118 @@ void pivot(t_stack *stacka, t_stack *stackb)
 			aux = aux_temp;
 	}
 	pivot(stacka, stackb);
-	low_number = lowest_number(stacka);
-	auxer = stacka->first;
-	while (!(ordenaded(stacka)))
-	{
-		if (auxer->data == low_number)
-			shift_up_ra(stacka);
-		else if (auxer->next->data < auxer->data)
-		{
-			swap_a(stacka);
-			shift_up_ra(stacka);
-		}
-		else
-			shift_up_ra(stacka);
-		auxer = stacka->first->next;
-	}
-	auxiliar = stackb->first;
-	while (auxiliar)
-	{
-		big_number = lowest_number(stackb);
-		if (big_number == auxiliar->data)
-		{
-			push_a(stacka, stackb);
-			shift_up_ra(stacka);
-		}
-		else 
-			shift_up_rb(stackb);
-		auxiliar = stackb->first;
-		if (auxiliar->next == NULL)
-			break;
-	}
 	return ;
 }
 
 void	quick_insert(t_stack *stacka,t_stack *stackb)
 {
 	t_node	*aux;
-	int	low_number;
+	int	big_number;
 
-	low_number = lowest_number(stackb);
+	big_number = bigest_number(stackb);
 	aux = stackb->first->next;
+	imprimir_pilha(stacka, 'a');
+	imprimir_pilha(stackb, 'b');
 	while (aux)
 	{
-		if (low_number == aux->data)
+		if (big_number == aux->data)
 		{
+			// push_rotate(stacka, stackb);
 			push_a(stacka, stackb);
-			shift_up_ra(stacka);
+			// shift_up_ra(stacka);
 		}
 		else
-			shift_up_rb(stackb);
-		low_number = lowest_number(stackb);
+			inverted_core_ordenation(stacka, stackb);
+			// shift_up_rb(stackb);
+		big_number = bigest_number(stackb);
 		aux = aux->next;
 	}
 	push_a(stacka, stackb);
-	shift_up_ra(stacka);
+	// shift_up_ra(stacka);
 }
+
+
+//pivot funcional com 100% porém muitos movimentos.
+// void pivot(t_stack *stacka, t_stack *stackb)
+// {
+//     t_node  *aux;
+//     t_node  *aux_temp;
+//     int     pivot_number;
+
+//     pivot_number = 0;
+//     aux = stacka->first->next;
+//     aux_temp = aux;
+// 	if (ordenaded(stacka))
+// 		return ;
+// 	if (stacka->first->data > lowest_number(stacka) && stacka->first->data < bigest_number(stacka))
+// 		pivot_number = stacka->first->data;
+// 	else
+//     {
+// 		swap_a(stacka);
+//         pivot_number = stacka->first->data;
+//     }
+// 	while (1)
+// 	{
+// 		if (ordenaded(stacka))
+// 		{
+// 			if (stackb->first != NULL)
+// 				core_ordenation(stacka, stackb);
+// 			break;
+// 		}
+// 		if (aux->next != NULL)
+// 			aux_temp = aux->next;
+// 		if (pivot_number < aux->data)
+// 		{
+// 			swap_a(stacka);
+// 			push_b(stacka, stackb);
+// 		}
+// 		else if (pivot_number == aux->data)
+// 			break;
+// 		else
+// 			shift_up_ra(stacka);
+// 		if (aux_temp == NULL)
+// 			break;
+// 		else
+// 			aux = aux_temp;
+// 	}
+// 	pivot(stacka, stackb);
+// 	return ;
+// }
+
+// void	quick_insert(t_stack *stacka,t_stack *stackb)
+// {
+// 	t_node	*aux;
+// 	int	low_number;
+
+// 	low_number = lowest_number(stackb);
+// 	aux = stackb->first->next;
+// 	while (aux)
+// 	{
+// 		if (low_number == aux->data)
+// 		{
+// 			push_rotate(stacka, stackb);
+// 			// push_a(stacka, stackb);
+// 			// shift_up_ra(stacka);
+// 		}
+// 		else
+// 			core_ordenation(stacka, stackb);
+// 			// shift_up_rb(stackb);
+// 		low_number = lowest_number(stackb);
+// 		aux = aux->next;
+// 	}
+// 	push_a(stacka, stackb);
+// 	shift_up_ra(stacka);
+// }
 
 void	quick_sort(t_stack *stacka, t_stack *stackb)
 {
-	pivot(stacka, stackb);
+	choice_pivot(stacka, stackb);
 	quick_insert(stacka, stackb);
+	// inverted_core_ordenation(stacka, stackb);
+	// returned_a (stacka, stackb);
+
+	imprimir_pilha(stacka, 'a');
+	imprimir_pilha(stackb, 'b');
+	// pivot(stacka, stackb);
+	// quick_insert(stacka, stackb);
 }
